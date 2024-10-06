ce0076c0817c4f7dc62ffb5803283a90e1907619
spuxx1701 <accounts@spuxx.dev>
2024-10-06 20:22:00 +0200

    feat: Update text

--- apps/spuxx-astro/src/consts.ts
+++ apps/spuxx-astro/src/consts.ts
@@ -1,6 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'spuxx.dev';
-export const SITE_DESCRIPTION =
-  "Hey! My name is Leo, and I tend to go by the name 'spuxx' in the world of software. This is my dev blog.";
+export const SITE_DESCRIPTION = "I'm spuxx. This is my website and shit.";
