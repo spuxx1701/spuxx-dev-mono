d5b5316d736367cedd280ce01421e85913d7f996
spuxx1701 <accounts@spuxx.dev>
2024-12-10 12:21:31 +0100

    docs(lists): Document `InviteLinkResource`

--- apps/spuxx-api/src/utils/invite-links/invite-link.resource.ts
+++ apps/spuxx-api/src/utils/invite-links/invite-link.resource.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class InviteLinkResource {
+  @ApiProperty({
+    description:
+      'The invite link. Any authenticated user opening this link will accept the invite.',
+  })
   link: string;
+  @ApiProperty({
+    description: 'The invite code. Part of the invite link.',
+  })
   code: string;
 }
