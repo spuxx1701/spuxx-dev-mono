83e5c46edf46fc89ca331f50ae9732b4c8b7f120
spuxx1701 <accounts@spuxx.dev>
2024-12-10 19:37:06 +0100

    style: Removed commented code

--- apps/toledo/src/services/dialog/dialog.service.ts
+++ apps/toledo/src/services/dialog/dialog.service.ts
@@ -1,8 +1,6 @@
 import { ServiceMixin } from '@spuxx/js-utils';
 import { ref, shallowRef } from 'vue';
 import type { DialogOptions } from './types';
-// import { DialogRegistry, type DialogKey } from './dialog.registry';
-
 export class Dialog extends ServiceMixin<Dialog>() {
   private static _activeDialogComponent = shallowRef<HTMLDialogElement | null>(null);
   private static _activeDialogOptions = ref<DialogOptions | null>(null);
@@ -20,13 +18,8 @@ export class Dialog extends ServiceMixin<Dialog>() {
    * @param key The key of the dialog to open.
    * @param options The dialog options.
    */
-  // static open<K extends DialogKey>(key: DialogKey, options: DialogRegistry[K]['options']) {
-  //   const dialogInfo = DialogRegistry[key];
-  //   const component = dialogInfo.component;
-  //   const defaultOptions = dialogInfo.options;
-  // }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static open<TOptions extends DialogOptions>(component: any, options: TOptions) {
-    console.log(component.__name); // Prints: ConfirmDialog
     this._activeDialogComponent.value = component;
     this._activeDialogOptions.value = options;
   }
