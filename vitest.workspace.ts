fa001eb1e3afb5c97a6bd8dccbc00893b1768055
spuxx1701 <accounts@spuxx.dev>
2024-12-15 17:10:27 +0100

    chore(vitest): Removed vitest workspace file

--- vitest.workspace.ts
+++
@@ -1,8 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-  './vite.config.nest.ts',
-  './vite.config.ts',
-  './apps/spuxx-api/vite.config.ts',
-  './apps/toledo/vite.config.ts',
-]);
